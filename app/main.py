11282790339d9a664e681b95642cb5c737df0b7b
Vamshi Krishna <pvamshi@gmail.com>
2018-08-04 20:16:28 +0530

    Simple todo api functional

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, Blueprint
+from flask_restplus import Resource, Api
+from app.todo.api import ns
+from app.api import api
+app = Flask(__name__)
+
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+if __name__ == '__main__':
+    blueprint = Blueprint('api', __name__, url_prefix='/api')
+    api.init_app(blueprint)
+    api.add_namespace(ns)
+    app.register_blueprint(blueprint)
+    app.run(debug=True)
